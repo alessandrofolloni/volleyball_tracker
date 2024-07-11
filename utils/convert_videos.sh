4843d0a58c53210481b51b1b088ff13b7c00f447
alessandrofolloni <allefollo@gmail.com>
2024-07-11 17:38:01 +0200

    ignore

--- utils/convert_videos.sh
+++ utils/convert_videos.sh
@@ -1,19 +1,22 @@
 #!/bin/bash
 
-# Specify the full path to your folder
 FOLDER_PATH="$HOME/Desktop/Unibo/PW ML4CV/videos"
-# Name of the subfolder for converted MP4 files
+# Nome della sottocartella per i file MP4 convertiti
 CONVERTED_FOLDER="Converted"
 
-# Create the Converted folder if it doesn't exist
 mkdir -p "$FOLDER_PATH/$CONVERTED_FOLDER"
 
-# Navigate to the folder containing the original files
-cd "$FOLDER_PATH" || exit 1 # Exit if the folder doesn't exist
+convert_files() {
+  local folder_path="$1"
+  # shellcheck disable=SC2095
+  find "$folder_path" -type f -name "*.m4v" | while read -r file; do
+    # Crea la stessa struttura di directory all'interno della cartella Converted
+    relative_path="${file#$FOLDER_PATH/}"
+    output_dir="$FOLDER_PATH/$CONVERTED_FOLDER/$(dirname "$relative_path")"
+    mkdir -p "$output_dir"
+    # Converti il file
+    ffmpeg -i "$file" -codec copy "$output_dir/${file%.m4v}.mp4"
+  done
+}
 
-# Convert each M4V file to MP4 and save in the Converted folder
-for file in *.m4v; do
-  ffmpeg -i "$file" -codec copy "$CONVERTED_FOLDER/${file%.m4v}.mp4"
-done
-
-
+convert_files "$FOLDER_PATH"
